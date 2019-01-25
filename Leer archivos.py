d5b7df4e6fa4eac46f422fde539a4d61eff52370
Alejandro Valverde Mahou <pheithar@hotmail.com>
2019-01-25 17:13:47 +0100

    Prueba

---
+++ Leer archivos.py
@@ -0,0 +1,17 @@
+print("Ejercicio: leer archivos")
+
+#archivoTexto = open("marcianitos.txt")
+#print(archivoTexto)
+
+#contenidoArchivo = archivoTexto.read()
+#print(contenidoArchivo)
+
+print(open("marcianitos.txt").read())
+contenido = open("marcianitos.txt").readlines()
+#print(contenido[1])
+print(len(contenido))
+
+i = 0
+while i < len(contenido):
+    print(contenido[i])
+    i+=1 
